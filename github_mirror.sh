0e6a96d8041ceb555b38903f1f328a29ae43e888
alexbega <alexiabega0930@gmail.com>
2023-01-15 10:39:34 +0100

    scripts

--- github_mirror.sh
+++
@@ -1,3 +0,0 @@
-git clone --bare https://github.com/alexiabega/test_1.git
-cd test_1.git
-git push --mirror https://gitlab.com/alexiabega0930/test_1.git
--- github_script.sh
+++ github_script.sh
@@ -1,1 +1,11 @@
-echo "hello from github"
+cd test_1
+if [ "git pull" == "Already up-to-date." ]
+then
+    echo "Nothing to update!"
+else
+    git pull
+    git push mirror
+fi
+
+git remote add mirror https://gitlab.com/alexiabega0930/test_1.git
+git push mirror
